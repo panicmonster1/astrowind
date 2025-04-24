9593b257d6abb5fd174aec63eea0157b14cbabb6
wangr222 <wangr222@chinaunicom.cn>
2025-04-25 00:16:20 +0800

    修改端口为443

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,9 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: 'static',  
+  output: 'static',
+  server: { port: 443 },
+  
   integrations: [
     tailwind({
       applyBaseStyles: false,
